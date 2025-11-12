47e6216111daeec52ffbfb05a02978be1331ab17
linikers <linikers@hotmail.com>
2025-11-12 15:06:08 -0300

    adminTst

--- src/pages/containerVotos/containerVotacao.tsx
+++ src/pages/containerVotos/containerVotacao.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Conteiner () {
-    ////deve jogar os componentes register e top100
-    // e calcular o percentual que carrega no vote
-    const [user, setUser] = useState();
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        const fetchUsers = async () => {
+// export default function Conteiner () {
+//     ////deve jogar os componentes register e top100
+//     // e calcular o percentual que carrega no vote
+//     const [user, setUser] = useState();
+//     const [loading, setLoading] = useState(false);
+//     useEffect(() => {
+//         const fetchUsers = async () => {
 
-            try {
+//             try {
                 
-            } catch (error) {
+//             } catch (error) {
                 
-            }
-        }
-    })
-}
+//             }
+//         }
+//     })
+// }
